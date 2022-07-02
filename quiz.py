2b8de107e4415f15e4178b5a39a9f9b064ce6f5e
Zhu Wentao <95064358+taoo0316@users.noreply.github.com>
2022-07-02 18:18:42 +0800

    Add files via upload

---
+++ quiz.py
@@ -0,0 +1,13 @@
+
+QUESTIONS = [
+    ("Where does Wentao go to school'", "Yale-NUS College"),
+    ("How old is Wentao", "22"),
+    ("What is Wentao's major in college", " Computer Science"),
+]
+
+for question, correct_answer in QUESTIONS:
+    answer = input(f"{question}? ")
+    if answer == correct_answer:
+        print("Correct!")
+    else:
+        print(f"The answer is {correct_answer!r}, not {answer!r}")
